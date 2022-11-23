f3ba41cf7831eb5ebb39e45bb3d9437915caa6ba
takahiro <takapi854@gmail.com>
2022-11-23 20:58:38 +0900

    push

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
     println!("Hello, world!");
+    let num = math(3,5);
+    let str_num = &num.to_string();
+    println!("{str_num}");
 }
 
-fn math(n: u64, m: u64) -> u64 {
+fn math(n: i64, m: i64) -> i64 {
     return n + m;  
 }
 
