932c752632d24be7b135f2b3fc78987b04b478f0
takahiro <takapi854@gmail.com>
2022-12-15 22:08:57 +0900

    push

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    response::Html, routing::{get, post}, Router, Json,
+    http::StatusCode,
+    response::IntoResponse,
+};
 use std::net::SocketAddr;
+use serde::{Deserialize, Serialize};
+
+
+
 
 #[tokio::main]  // main関数を非同期関数にするために必要
 async fn main() {
@@ -8,7 +16,9 @@ async fn main() {
     }
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/users", post(create_user));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
@@ -21,3 +31,35 @@ async fn main() {
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
 }
+
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> impl IntoResponse {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
+
