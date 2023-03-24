5140fd9fea282faaf608f3d0b3a58283da650c87
takahiro <takapi854@gmail.com>
2023-03-24 23:16:27 +0900

    update: path param

--- rust-axum-driver/src/routes/user/user.rs
+++ rust-axum-driver/src/routes/user/user.rs
@@ -1,7 +1,8 @@
+use axum::extract::Path;
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use rust_axum_kernel::model::user;
 
-pub async fn get_user(
+pub async fn create_user(
     // this argument tells axum to parse the request body
     // as JSON into a `CreateUser` type
     Json(payload): Json<user::RequestUser>,
@@ -15,4 +16,13 @@ pub async fn get_user(
     // this will be converted into a JSON response
     // with a status code of `201 Created`
     (StatusCode::CREATED, Json(user))
+}
+
+pub async fn get_user(Path(user_id): Path<String>) -> impl IntoResponse {
+    let user = user::User {
+        id: user_id.to_owned(),
+        name: format!("name{}", user_id.to_owned()),
+    };
+
+    (StatusCode::OK, Json(user))
 }
