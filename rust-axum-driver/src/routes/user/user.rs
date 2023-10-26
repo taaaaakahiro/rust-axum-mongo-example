c2b7f4bdc000cb873722751c1e5ff6ed7f817d28
takahiro <takapi854@gmail.com>
2023-10-26 11:56:36 +0900

    fix: driver/route/user

--- rust-axum-driver/src/routes/user/user.rs
+++ rust-axum-driver/src/routes/user/user.rs
@@ -8,23 +8,13 @@ pub async fn post_user(
     Json(_payload): Json<RequestUser>,
 ) -> impl IntoResponse {
     // insert your application logic here
-    // let user = User {
-    //     id: payload.id,
-    //     name: payload.name,
-    // };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
+    //TODO: 後で
+    tracing::info!("success!");
     (StatusCode::CREATED, Json("success"))
 }
 
 pub async fn get_user(Path(_user_id): Path<String>) -> impl IntoResponse {
-    // let user = User {
-    //     id: user_id.to_owned(),
-    //     name: format!("name{}", user_id.to_owned()),
-    // };
-
-    tracing::info!("request success!");
-
+    //TODO: 後で
+    tracing::info!("success!");
     (StatusCode::OK, Json("success"))
 }
